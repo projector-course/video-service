d947a645ae4933907d753a04a69071116e20845d
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2021-11-05 11:57:39 +0200

    fix: add ffmpeg installer

--- src/api/controllers/convert_video.js
+++ src/api/controllers/convert_video.js
@@ -1,9 +1,9 @@
 const FF = require('fluent-ffmpeg');
+const FFpath = require('@ffmpeg-installer/ffmpeg').path;
 const fs = require('fs');
 
-const pathToFfmpeg = 'D:\\Program Files\\ffmpeg-n4.4.1-win64-gpl-4.4\\bin\\ffmpeg.exe';
-
-FF.setFfmpegPath(pathToFfmpeg);
+console.log('PATH TO FFMPEG:', FFpath);
+FF.setFfmpegPath(FFpath);
 
 const convertVideo = (input, output, from, to) => new Promise((resolve, reject) => {
   if (from === to) {
