c9454e4e940269dadd3453cc25c2817de3acfd87
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2021-12-30 17:33:16 +0200

    fix: add exchange assertion

--- src/services/amqp.js
+++ src/services/amqp.js
@@ -1,6 +1,8 @@
 const rabbit = require('amqplib');
 const { getModuleLogger } = require('./logService');
-const { AMQP_HOST, AMQP_EXCHANGE_NAME, AMQP_EVENT } = require('./configService');
+const {
+  AMQP_HOST, AMQP_EXCHANGE_NAME, AMQP_EXCHANGE_TYPE, AMQP_EVENT, AMQP_QUEUE_NAME,
+} = require('./configService');
 
 const logger = getModuleLogger(module);
 logger.debug('service CREATED');
@@ -8,8 +10,11 @@ logger.debug('service CREATED');
 const amqp = rabbit
   .connect(AMQP_HOST)
   .then((connection) => connection.createChannel())
-  .then((channel) => {
+  .then(async (channel) => {
     logger.debug('AMQP channel created');
+    await channel.assertExchange(AMQP_EXCHANGE_NAME, AMQP_EXCHANGE_TYPE);
+    const queue = await channel.assertQueue(AMQP_QUEUE_NAME);
+    await channel.bindQueue(queue.queue, AMQP_EXCHANGE_NAME, AMQP_EVENT);
     return channel;
   });
 
